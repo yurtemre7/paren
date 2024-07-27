7a2e7af577f8e5c27910c2ad942510de0181035a
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-07-27 20:06:21 +0200

    update release script

--- release-emre.sh
+++ release-emre.sh
@@ -1,5 +1,10 @@
+git pull
 flutter clean && flutter pub get
 flutter build ios --release
 cd ios || exit
 fastlane release
 cd .. || exit
+git add .
+git commit -m "update ios build number"
+git push
+git pull
