82d14470adb3c76b712727edfe423afbd03f67eb
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-08-03 13:55:41 +0200

    update web

--- release-web.sh
+++ release-web.sh
@@ -5,7 +5,7 @@ git_count=$(git rev-list --count HEAD)
 flutter clean && flutter pub get
 flutter build web --build-number="$git_count"
 rsync -avhz ./build/web/ ./docs/
-git add .
+git add ./docs/
 git commit -m "update website"
 git push
 git pull
