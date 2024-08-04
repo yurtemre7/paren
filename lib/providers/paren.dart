2a023a9bf8f499f9f46da9958416e7206884ce18
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-08-04 16:32:08 +0200

    remove redundant info

--- lib/providers/paren.dart
+++ lib/providers/paren.dart
@@ -28,7 +28,6 @@ class Paren extends GetxController {
     paren.sp = await SharedPreferences.getInstance();
     await paren.initCurrencies();
     await paren.initSettings();
-    logMessage('Initialized Paren');
     return paren;
   }
 
