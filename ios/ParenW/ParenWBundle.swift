1e220c8678fc4f86ab53daf1972c0d86b559d7b6
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-08-09 15:23:00 +0200

    add widget to ios

---
+++ ios/ParenW/ParenWBundle.swift
@@ -0,0 +1,17 @@
+//
+//  ParenWBundle.swift
+//  ParenW
+//
+//  Created by Emre Yurtseven on 09.08.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ParenWBundle: WidgetBundle {
+    var body: some Widget {
+        ParenW()
+        ParenWLiveActivity()
+    }
+}
