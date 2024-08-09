b03cc75dcdffe43dedff9374618e253c144a7708
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-08-09 15:25:45 +0200

    update build script

--- release-ios.sh
+++ release-ios.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull
 git_count=$(git rev-list --count main)
 
 flutter clean && flutter pub get
@@ -6,3 +7,7 @@ flutter build ios --release --build-number="$git_count"
 cd ios || exit
 fastlane release
 cd .. || exit
+git add ./ios/
+git commit -m "update ios version"
+git push
+git pull
