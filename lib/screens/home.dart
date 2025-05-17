cd83de87e493e1c462acade9dcbc04cdf4b88e7d
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-05-17 12:31:49 +0200

    refactor: update scroll control behavior for modal based on platform

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -490,7 +490,7 @@ class _HomeState extends State<Home> {
                 ),
               ),
             ),
-            isScrollControlled: true,
+            isScrollControlled: !(GetPlatform.isIOS || GetPlatform.isAndroid),
           );
         },
       ),
