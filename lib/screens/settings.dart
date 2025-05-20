2859f653ce0d426bbc1d78427a712d266ad67a04
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-05-21 00:12:28 +0200

    refactor: remove unused import from settings.dart

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:paren/providers/extensions.dart';
 import 'package:paren/providers/paren.dart';
