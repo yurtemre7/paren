2da0e8f8760ee4d0ad78fb6f636aa432982ddd12
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-05-22 20:38:50 +0200

    try to optimize loads and saves, also fix widget currency formatting

--- lib/providers/paren.dart
+++ lib/providers/paren.dart
@@ -26,7 +26,6 @@ class Paren extends GetxController {
   final fromCurrency = 'jpy'.obs;
   final toCurrency = 'eur'.obs;
 
-  final autofocusTextField = false.obs;
   final appColor = Colors.orange.getValue.obs;
   final appThemeMode = ThemeMode.system.obs;
   final conv1Size = 20.0.obs;
@@ -40,8 +39,11 @@ class Paren extends GetxController {
   Paren();
 
   Future<void> init() async {
-    await initCurrencies();
-    await initFavorites();
+    await Future.wait([
+      initCurrencies(),
+      initFavorites(),
+    ]);
+
     updateWidgetData();
   }
 
@@ -50,7 +52,6 @@ class Paren extends GetxController {
     latestTimestamp.value = DateTime.now();
     fromCurrency.value = 'eur';
     toCurrency.value = 'jpy';
-    autofocusTextField.value = false;
     appColor.value = Colors.orange.getValue;
     appThemeMode.value = ThemeMode.system;
     conv1Size.value = 20.0;
@@ -106,30 +107,31 @@ class Paren extends GetxController {
 
   Future<void> updateCurrencies() async {
     var currencyList = currencies.map((e) => json.encode(e.toJson())).toList();
-    await sp.setStringList('currencies', currencyList);
-
     latestTimestamp.value = DateTime.now();
-    await sp.setString('latestTimestamp', latestTimestamp.value.toString());
+    await Future.wait([
+      sp.setStringList('currencies', currencyList),
+      sp.setString('latestTimestamp', latestTimestamp.value.toString()),
+    ]);
   }
 
   Future<void> updateDefaultConversion() async {
-    await sp.setString('fromC', fromCurrency.value.toLowerCase());
-    await sp.setString('toC', toCurrency.value.toLowerCase());
+    await Future.wait([
+      sp.setString('fromC', fromCurrency.value.toLowerCase()),
+      sp.setString('toC', toCurrency.value.toLowerCase()),
+    ]);
   }
 
   Future<void> saveSettings() async {
-    await sp.setBool('autofocusTextField', autofocusTextField.value);
-    await sp.setInt('appColor', appColor.value);
-    await sp.setInt('appThemeMode', appThemeMode.value.index);
-    await sp.setDouble('conv1Size', conv1Size.value);
-    await sp.setDouble('conv2Size', conv2Size.value);
-    await sp.setDouble('calculatorInputHeight', calculatorInputHeight.value);
+    await Future.wait([
+      sp.setInt('appColor', appColor.value),
+      sp.setInt('appThemeMode', appThemeMode.value.index),
+      sp.setDouble('conv1Size', conv1Size.value),
+      sp.setDouble('conv2Size', conv2Size.value),
+      sp.setDouble('calculatorInputHeight', calculatorInputHeight.value),
+    ]);
   }
 
   Future<void> initSettings() async {
-    var autofocusValue = await sp.getBool('autofocusTextField');
-    autofocusTextField.value = autofocusValue ?? false;
-
     var appColorValue = await sp.getInt('appColor');
     appColor.value = appColorValue ?? Colors.orange.getValue;
 
@@ -196,45 +198,40 @@ class Paren extends GetxController {
       return;
     }
     logMessage('Updating widget');
-    int selectedFromCurrencyIndex = 0;
-    int selectedToCurrencyIndex = 0;
-    var idxFrom = currencies.indexWhere((currency) => currency.id == this.fromCurrency.value);
-    if (idxFrom != -1) {
-      selectedFromCurrencyIndex = idxFrom;
-    }
-    var idxTo = currencies.indexWhere((currency) => currency.id == this.toCurrency.value);
-    if (idxTo != -1) {
-      selectedToCurrencyIndex = idxTo;
-    }
-    var fromCurrency = currencies[selectedFromCurrencyIndex];
-    var toCurrency = currencies[selectedToCurrencyIndex];
+
+    var fromCurrency = currencies.firstWhere(
+      (currency) => currency.id == this.fromCurrency.value,
+      orElse: () => currencies.first,
+    );
+    var toCurrency = currencies.firstWhere(
+      (currency) => currency.id == this.toCurrency.value,
+      orElse: () => currencies.first,
+    );
 
     var fromRate = fromCurrency.rate;
     var toRate = toCurrency.rate;
 
     var inputConverted = 1.0;
-
     var convertedAmount = 1.0 * toRate / fromRate;
-
     var reConvertedAmount = 1.0 * fromRate / toRate;
 
-    var roundedTo = (convertedAmount * 100).round() / 100;
-    var reRoundedTo = (reConvertedAmount * 100).round() / 100;
-    var amountStr = roundedTo.toStringAsFixed(2).replaceAllMapped(
-          RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
-          (Match m) => '${m[1]},',
-        );
-    var reAmountStr = reRoundedTo.toStringAsFixed(2).replaceAllMapped(
-          RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
-          (Match m) => '${m[1]},',
-        );
-    var inputStr = inputConverted.toStringAsFixed(2).replaceAllMapped(
-          RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
-          (Match m) => '${m[1]},',
-        );
+    NumberFormat numberFormatFrom = NumberFormat.simpleCurrency(
+      name: fromCurrency.id.toUpperCase(),
+    );
+    NumberFormat numberFormatRe = NumberFormat.simpleCurrency(
+      name: fromCurrency.id.toUpperCase(),
+    );
+    NumberFormat numberFormatTo = NumberFormat.simpleCurrency(
+      name: toCurrency.id.toUpperCase(),
+    );
 
-    var priceString = '$inputStr ${fromCurrency.symbol} ➜ $amountStr ${toCurrency.symbol}';
-    var priceReString = '$inputStr ${toCurrency.symbol} ➜ $reAmountStr ${fromCurrency.symbol}';
+    String amountStr = numberFormatTo.format(convertedAmount);
+    String reAmountStr = numberFormatRe.format(reConvertedAmount);
+    String inputStr = numberFormatFrom.format(inputConverted);
+    String inputStrRe = numberFormatTo.format(inputConverted);
+
+    var priceString = '$inputStr ➜ $amountStr';
+    var priceReString = '$inputStrRe ➜ $reAmountStr';
 
     await Future.wait([
       HomeWidget.saveWidgetData(
