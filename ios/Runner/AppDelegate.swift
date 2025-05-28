f47552f257687a07cff5e1a93987d72796b3c615
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-05-28 22:07:28 +0200

    feat: add widget configuration and deep linking support

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import home_widget
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,11 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 17.0, *) {
+      HomeWidgetPlugin.setConfigurationLookup(to: [
+          "ConfigurableWidget": ConfigurationAppIntent.self
+      ])
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
