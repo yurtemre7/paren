d3b4dc3e5fd9b8ce5d5c9dbcc38e606bccd16a9a
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-07-05 13:34:45 +0200

    update website

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1035199077"
+    serviceWorkerVersion: "1141842540"
   }
 });
