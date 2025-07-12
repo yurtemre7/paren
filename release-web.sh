d3e57ab52367e58ca1d709faeb153930fc7d562f
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-07-12 16:47:33 +0200

    use wasm for web performance

--- release-web.sh
+++ release-web.sh
@@ -3,7 +3,7 @@ git pull
 git_count=$(git rev-list --count HEAD)
 
 flutter clean && flutter pub get
-flutter build web --build-number="$git_count"
+flutter build web --build-number="$git_count" --wasm
 rsync -avhz ./build/web/ ./docs/
 git add ./docs/
 git commit -m "update website"
