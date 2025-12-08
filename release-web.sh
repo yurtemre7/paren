a28d3ed3a696bb411d71bf4c08d2785dbfcf5201
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-12-08 17:34:17 +0100

    update web base href and release process

--- release-web.sh
+++ release-web.sh
@@ -4,8 +4,10 @@ git_count=$(git rev-list --count HEAD)
 
 flutter clean && flutter pub get
 flutter build web --build-number="$git_count" --wasm
-rsync -avhz ./build/web/ ./docs/
-git add ./docs/
+rsync -avhz ./build/web/ /Users/yurtemre/Code/FlutterProjects/yurtemre_deno/static/iparen
+cd /Users/yurtemre/Code/FlutterProjects/yurtemre_deno/
+git pull
+git add .
 git commit -m "update website"
 git push
 git pull
