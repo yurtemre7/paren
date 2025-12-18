9f67ef8f0dad72f4747a67266404fab01219c2de
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-12-18 23:12:45 +0100

    remove

--- lib/components/sheet_form_bottom_sheet.dart
+++ lib/components/sheet_form_bottom_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:paren/classes/sheet.dart';
 import 'package:paren/components/currency_changer_row.dart';
