4c844177aa27b6f0fbc9deb9edccff8874cd56fd
Emre Yurtseven <emreyurtseven18@gmail.com>
2026-01-10 13:23:38 +0100

    update for new IUSceneDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,17 +3,20 @@ import UIKit
 import home_widget
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
     if #available(iOS 17.0, *) {
       HomeWidgetPlugin.setConfigurationLookup(to: [
           "ConfigurableWidget": ConfigurationAppIntent.self
       ])
     }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
