6aed5187207882893e66bb09dae0d7ed34cbcfa0
Emre Yurtseven <emreyurtseven18@gmail.com>
2026-06-14 17:58:42 +0200

    feat: fix macOS .app path

--- release-github-darwin.sh
+++ release-github-darwin.sh
@@ -15,7 +15,7 @@ flutter build ipa --release --build-number="$git_count"
 flutter build macos --release --build-number="$git_count"
 
 IPA_PATH="$(find build/ios/ipa -maxdepth 1 -type f -name '*.ipa' -print0 | xargs -r -0 ls -t | head -n 1)"
-APP_PATH="$(find build/macos/Build/Products/Release -maxdepth 1 -type f -name '*.app' -print0 | xargs -r -0 ls -t | head -n 1)"
+APP_PATH="$(find build/macos/Build/Products/Release -maxdepth 1 -name '*.app' -print)"
 
 if [[ -z "$IPA_PATH" || ! -f "$IPA_PATH" ]]; then
   echo "Error: no IPA found in build/ios/ipa." >&2
